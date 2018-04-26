8be57346cb70ac69b62a273ae838ad44e11d0d44
leelle0117 <mklee@lee-and.com>
2018-04-26 22:51:49 +0900

    block_01

--- block.rb
+++ block.rb
@@ -1,24 +1,33 @@
 require 'digest'
 
-class BlockChain
+class Blockchain
 
 	def initialize
-		@number_of_blocks = 1
+		@chain = []
+
  	end
 		
 	def mining
-		@number_of_blocks = @number_of_blocks + 1
-		history = []
         begin
         	nonce = rand(100)
-        	hashed = Digest::SHA256.hexdigest(nonce.to_s)
-        	history << nonce
-		end while hashed[0..3] != '00'
-		history
+		end while nonce != 0
+		
+		block = {
+			'index' => @chain.length + 1,
+			'time' => Time.now,
+			'nonce' => nonce,
+			'previous_block' => Digest::SHA256.hexdigest(last_block.to_s)
+
+		}
+		@chain << block
 	end
 
-	def my_blocks
-		@number_of_blocks
+	def current_chain
+		@chain
+end
+
+	def last_block
+		@chain[-1]
 	end
 end
 
--- server.rb
+++ server.rb
@@ -3,15 +3,25 @@
 require 'sinatra'
 require './block'
 
-b = BlockChain.new
+b = Blockchain.new
 
 
 get '/' do
-	"Total number of blocks : " + b.my_blocks.to_s
+	message = ""
+	b.current_chain.each do |c|
+	message << "번호는 " + c['index'].to_s + " 입니다. <br>"	
+	message << "Nonce는 " + c['nonce'].to_s + " 입니다. <br>"
+	message << "시간은 " + c['time'].to_s + " 입니다. <br>"	
+	message << "앞 주소는 " + c['previous_block'].to_s + " 입니다. <br>"
+	message << "내 주소는 " + Digest::SHA256.hexdigest(c.to_s) + " 입니다. <br>"	
+	message << "<hr>"
 end
+	message
+end	
 
 get '/mine' do
-	b.mining.to_s
+	b.mining
+	"블럭을 찾았다!"
 end
 
 
