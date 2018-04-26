7368138c821efd89c1970d37b4fb72a3ee2e66de
leelle0117 <mklee@lee-and.com>
2018-04-26 19:03:56 +0900

    mklee3

--- block.rb
+++ block.rb
@@ -2,6 +2,10 @@ class Dog
 
 	def initialize
 		@weight = 0.5
+		@height = 3.0
+		@age = 1
+		@university =""
+		@name = ""
 
 	end	
 
@@ -10,6 +14,21 @@ class Dog
 		puts @weight
 	end	
 
+	def run
+		@height = @height - 1.5
+		puts @height
+	end	
+
+	def naming
+		@name = "Ann"
+		puts @name
+	end	
+
+
+	def birth
+		@age = @age + 1
+		puts @age
+	end	
 
 end
 
@@ -17,9 +36,19 @@ end
 bbb = Dog.new
 cc = Dog.new
 
+bbb.naming
 bbb.eat
 bbb.eat
-bbb.eat
+bbb.run
+bbb.run
+bbb.run
+bbb.birth
+bbb.birth
+bbb.birth
 
-cc.eat
-
+cc.naming
+cc.run
+cc.birth
+cc.birth
+cc.birth
+cc.birth
