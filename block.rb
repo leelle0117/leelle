367435d022a3e0b66b6f82b90df19d675f671563
leelle0117 <mklee@lee-and.com>
2018-04-26 18:54:01 +0900

    mklee3

--- block.rb
+++ block.rb
@@ -1,1 +1,25 @@
-adfdfadff 
+class Dog
+
+	def initialize
+		@weight = 0.5
+
+	end	
+
+	def eat
+		@weight = @weight + 0.5
+		puts @weight
+	end	
+
+
+end
+
+
+bbb = Dog.new
+cc = Dog.new
+
+bbb.eat
+bbb.eat
+bbb.eat
+
+cc.eat
+
